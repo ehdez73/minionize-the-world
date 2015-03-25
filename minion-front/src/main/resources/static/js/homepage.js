31d752aae8db6e487f46c05ef117aad35bdb60a5
Ernesto Hernandez <ehdez73@gmail.com>
2015-03-25 20:26:12 +0100

    Add fail() callbacks to ajax calls

--- minion-front/src/main/resources/static/js/homepage.js
+++ minion-front/src/main/resources/static/js/homepage.js
@@ -7,17 +7,26 @@ $(document)
       $.ajax({
         url: "/api/ipsum/minion-ipsum",
         context: document.body
+      
       }).done(function(data) {
     	  $("#ipsum").text(data)
+      
+      }).fail(function(){
+        $("#ipsum").text("Ahhhhh! No banana")
       })
       
       // minion-character
       $.ajax({
          url: "/api/characters/minion-character",
          context: document.body
+      
       }).done(function(data) {
     	  $("#minion-name").text(data.name);
     	  $("#minion-image").attr("src", data.image).attr("alt", data.name);
+      
+      }).fail(function() {
+        $("#minion-name").text("Evil");
+        $("#minion-image").attr("src", "img/minion-evil.png").attr("alt", "Evil");
       })
 
 
