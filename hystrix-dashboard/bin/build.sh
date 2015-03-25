600c8feff220e893944aedb2112cf8d5d98a7d14
Ernesto Hernandez <ehdez73@gmail.com>
2015-03-25 20:25:05 +0100

    Delete hystrix-dashboard

--- hystrix-dashboard/bin/build.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-mvn package
-docker build -t ehdez73/hystrix .
-docker run -d -p 7979:7979 --name="hystrix" --hostname="hystrix" ehdez73/hystrix
--- hystrix-dashboard/build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-mvn package
-docker stop hystrix-dashboard
-docker rm hystrix-dashboard
-docker build --force-rm=true --tag=ehdez73/hystrix-dashboard .
-docker run -d -p 7979:7979 --name="hystrix-dashboard" --hostname="hystrix-dashboard" ehdez73/hystrix-dashboard
