ccc274e1f80eadc12e4482b4616ea1bd08edffbf
Ernesto Hdez <ehdez73@gmail.com>
2015-04-16 22:28:47 +0200

    Bugfix: Add link to config-server in minion-front/build.sh

--- minion-front/build.sh
+++ minion-front/build.sh
@@ -6,6 +6,7 @@ docker build --force-rm=true --tag=ehdez73/minion-front .
 docker run -d -p 8080:8080 \
    --name="minion-front" \
    --hostname="minion.front.machine" \
+   --link config-server:minion.config.machine \
    --link eureka-server:minion.eureka.machine \
    --link minion-characters:minion.characters.machine \
    --link minion-ipsum:minion.ipsum.machine \
